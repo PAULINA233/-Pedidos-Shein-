cdf186d87911086c0de643b232e39323afa2ddfd
PAULINA233 <66274346+PAULINA233@users.noreply.github.com>
2020-11-12 21:19:30 -0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+<hr/>
+Quejas y Sujerencias &copy; Con Paulina Tapia y Julisa Lizarraga <?=date("Y")?>
+
+</footer>
+
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Registrar pedido</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+    <form method="post">
+    	<h1>¡Pedidos Shein!</h1>
+    	<input type="text" name="name" placeholder="Prenda o Producto">
+    	<input type="text" name="name" placeholder="Cantidades">
+    	<input type="submit" name="register">
+    </form>
+        <?php 
+		include("registrar.php");
+		
+		
+		
+
+		?>
+</body>
+</html>
